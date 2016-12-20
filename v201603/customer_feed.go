ea9b3666ada7f44e4757b80c571749b26db1a8c8
Brian Bachtel <bbachtel@redventures.com>
2016-12-20 14:36:39 -0500

    remove v201603

--- v201603/customer_feed.go
+++
@@ -1,9 +0,0 @@
-package v201603
-
-type CustomerFeedService struct {
-	Auth
-}
-
-func NewCustomerFeedService(auth *Auth) *CustomerFeedService {
-	return &CustomerFeedService{Auth: *auth}
-}
--- v201603/feed.go
+++
@@ -1,9 +0,0 @@
-package v201603
-
-type FeedService struct {
-	Auth
-}
-
-func NewFeedService(auth *Auth) *FeedService {
-	return &FeedService{Auth: *auth}
-}
