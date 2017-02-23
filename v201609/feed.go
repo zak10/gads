242aaba558f0ec82cc2f727492ad81296f74facc
Dmitry Rassakhatsky <drassakhatsky@redventures.com>
2017-02-23 16:38:07 -0500

    Added FeedType

--- v201609/feed.go
+++ v201609/feed.go
@@ -7,3 +7,8 @@ type FeedService struct {
 func NewFeedService(auth *Auth) *FeedService {
 	return &FeedService{Auth: *auth}
 }
+
+// https://developers.google.com/adwords/api/docs/reference/v201609/AdGroupExtensionSettingService.Feed.Type
+// Feed hard type. Values coincide with placeholder type id.
+// Enum: NONE, SITELINK, CALL, APP, REVIEW, AD_CUSTOMIZER, CALLOUT, STRUCTURED_SNIPPET, PRICE
+type FeedType string
