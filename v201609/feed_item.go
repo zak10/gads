8af51341f384549b6bb5a9680897cc8accec4865
Dmitry Rassakhatsky <drassakhatsky@redventures.com>
2017-02-23 16:27:25 -0500

    Added FeedItemStatus

--- v201609/feed_item.go
+++ v201609/feed_item.go
@@ -7,3 +7,7 @@ type FeedItemService struct {
 func NewFeedItemService(auth *Auth) *FeedItemService {
 	return &FeedItemService{Auth: *auth}
 }
+
+// https://developers.google.com/adwords/api/docs/reference/v201609/AdGroupExtensionSettingService.FeedItem.Status
+// ENABLED, REMOVED, UNKNOWN
+type FeedItemStatus string
