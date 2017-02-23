cb8801b21edf4ba2df91cee4a459f3c3831ac544
Dmitry Rassakhatsky <drassakhatsky@redventures.com>
2017-02-23 16:29:59 -0500

    Added FeedItemDevicePreference

--- v201609/feed_item.go
+++ v201609/feed_item.go
@@ -11,3 +11,9 @@ func NewFeedItemService(auth *Auth) *FeedItemService {
 // https://developers.google.com/adwords/api/docs/reference/v201609/AdGroupExtensionSettingService.FeedItem.Status
 // ENABLED, REMOVED, UNKNOWN
 type FeedItemStatus string
+
+// https://developers.google.com/adwords/api/docs/reference/v201609/AdGroupExtensionSettingService.FeedItemDevicePreference
+// Represents a FeedItem device preference
+type FeedItemDevicePreference struct {
+	DevicePreference int64 `xml:"https://adwords.google.com/api/adwords/cm/v201609 devicePreference,omitempty"`
+}
